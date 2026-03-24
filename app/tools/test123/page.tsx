c0196c473f3bb85afdac9ea833a453528e81ca4d
Norman de Silva <normandesilva@Normans-MacBook-Pro.local>
2026-03-23 20:33:14 -0400

    Add simple test page

---
+++ app/tools/test123/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', color: 'white', background: '#000' }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, new pages are deploying correctly.</p>
+    </div>
+  );
+}
